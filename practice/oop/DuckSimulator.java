920aaf6c1d14fc3b1b0ad0e162198b702bd1dcb5
Anggasukhapratama <118103208+Anggasukhapratama@users.noreply.github.com>
2023-09-30 22:25:54 +0700

    DuckSimulator.java

--- practice/oop/DuckSimulator.java
+++ practice/oop/DuckSimulator.java
@@ -15,31 +15,44 @@ public class DuckSimulator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("mau pilih yang mana :");
-        System.out.println("1. Mallard Duck :)");
-        System.out.println("2. Rubber Duck :)");
-        System.out.println("3. Decoy Duck :)");
-        System.out.println("4. Model Duck :)");
-        int pilih_bebek = scanner.nextInt();
+        do {
+            System.out.println("Pilih jenis bebek:");
+            System.out.println("1. Mallard Duck");
+            System.out.println("2. Rubber Duck");
+            System.out.println("3. Decoy Duck");
+            System.out.println("4. Model Duck");
+            System.out.println("0. Keluar");
+            int pilihan = scanner.nextInt();
 
-        Duck duck = null;
+            if (pilihan == 0) {
+                System.out.println("Terima kasih. Keluar dari program.");
+                break;
+            }
 
-        if (pilih_bebek == 1) {
-            duck = new MallardDuck();
-        } else if (pilih_bebek == 2) {
-            duck = new RubberDuck();
-        } else if (pilih_bebek == 3) {
-            duck = new DecoyDuck();
-        } else if (pilih_bebek == 4) {
-            duck = new ModelDuck();
-        } else {
-            System.out.println("Nomer yang anda maksud tidak ada");
-            return;
-        }
+            Duck duck = null;
 
-        System.out.println("Pilihan anda : " + duck.getClass().getSimpleName());
+            switch (pilihan) {
+                case 1:
+                    duck = new MallardDuck();
+                    break;
+                case 2:
+                    duck = new RubberDuck();
+                    break;
+                case 3:
+                    duck = new DecoyDuck();
+                    break;
+                case 4:
+                    duck = new ModelDuck();
+                    break;
+                default:
+                    System.out.println("Pilihan yang Anda masukkan tidak valid");
+            }
 
-        duck.performQuack();
-        duck.performFly();
+            if (duck != null) {
+                System.out.println("Anda memilih: " + duck.getClass().getSimpleName());
+                duck.performQuack();
+                duck.performFly();
+            }
+        } while (true);
     }
 }
